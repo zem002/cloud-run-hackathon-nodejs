6f83dea8e6205ea6ae4ea6c3f38f2b7c47131e28
zem002 <88871969+zem002@users.noreply.github.com>
2022-08-26 15:54:10 +1000

    Update web.js

--- web.js
+++ web.js
@@ -13,7 +13,7 @@ app.post('/', function (req, res) {
   try{
     selfLink = req.body._links.self.href;
     arenaDimenions = req.body.arena.dims;
-    states = req.body.arena.states;
+    states = req.body.arena.state;
   }catch(err){
     console.log(err);
     const moves = ['F', 'T', 'L', 'R'];
@@ -22,6 +22,7 @@ app.post('/', function (req, res) {
   console.log(selfLink);
   console.log(arenaDimenions)
   console.log(states)
+  console.log(states[selfLink])
   
 });
 
